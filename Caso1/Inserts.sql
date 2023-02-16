095f3abf5d7f756d9137b6de90eec4a144d95279
MarioBarbozaA <marionetabar1@gmail.com>
2023-02-16 16:53:10 -0600

    Update Inserts.sql

--- Caso1/Inserts.sql
+++ Caso1/Inserts.sql
@@ -1,29 +1,94 @@
+-- ------------------------------------------------------------------------
+/*									TypeContact           			*/
+-- ------------------------------------------------------------------------
+
+INSERT INTO [Ferianueva].[dbo].[ContactInfo] 
+([tipoContactoId],[contacto])
+VALUES 
+('Phone')
+('Email')
+
+-- ------------------------------------------------------------------------
+/*									ContactInfo     					*/
+-- ------------------------------------------------------------------------
+INSERT INTO [Ferianueva].[dbo].[ContactInfo] 
+([tipoContactoId],[contacto])
+VALUES 
+(1,'80080268'),
+(2,'GraStarcher@gmail.com'),
+(1,'83365631'),
+(2, 'TinJones@gmail.com'),
+(1,'89956157'),
+(2,'EvaReichenbach@gmail.com'),
+(1,'82806739'),
+(2,'RicRush@gmail.com'),
+(1, '83714516'),
+(2, 'LawBernhart@gmail.com'),
+(1, '82688393'),
+(2, 'JoaGosselin@gmail.com'),
+(1, '81370699'),
+(2, 'JasHoag@gmail.com'),
+(1, '86617568'),
+(2, 'DavTung@gmail.com'),
+(1, '89874539'),
+(2, 'ChrSabino@gmail.com'),
+(1, '89874539'),
+(2, 'JefGuerrero@gmail.com'),
+(1, '82385652'),
+(2, 'RayFoust@gmail.com'),
+(1, '86830732'),
+(2, 'DeaConsidine@gmail.com'),
+(1, '89605250'),
+(2,  'TerArnold@gmail.com'),
+(1, '86079107'),
+(2,  'NinWesterman@gmail.com'),
+(1, '85371669'),
+(2, 'AshMckinley@gmail.com'),
+(1, '81029918'),
+(2, 'JoyHathaway@gmail.com'),
+(1, '85133678'),
+(2, 'LorBullock@gmail.com'),
+(1, '84914047'),
+(2,  'JoaJones@gmail.com'),
+(1, '89180931'),
+(2,  'KevKent@gmail.com'),
+(1, '85096644'),
+(2,  'CheWyman@gmail.com');
+
+
+
+
+
+-- ------------------------------------------------------------------------
+/*									ContactInfoByClient					*/
+-- ------------------------------------------------------------------------
+
 
 -- ------------------------------------------------------------------------
 /*									CLIENTES							*/
 -- ------------------------------------------------------------------------
 insert into dbo.Clientes (dbo.Clientes.nombre, dbo.Clientes.apellido1, dbo.Clientes.apellido2, dbo.Clientes.celular,
 dbo.Clientes.email) values 
-('Grace','Starcher','Parker',80080268, 'GraStarcher@gmail.com'),
- ('Tiny','Jones','Mansfield',83365631, 'TinJones@gmail.com'),
- ('Evan','Reichenbach','Tran',89956157, 'EvaReichenbach@gmail.com'),
- ('Richard','Rush','Gartrell',82806739, 'RicRush@gmail.com'),
- ('Lawrence','Bernhart','Settles',83714516, 'LawBernhart@gmail.com'),
- ('Joan','Gosselin','Griffin',82688393, 'JoaGosselin@gmail.com'),
- ('Jason','Hoag','Daley',81370699, 'JasHoag@gmail.com'),
- ('David','Tung','Sharp',86617568, 'DavTung@gmail.com'),
- ('Christina','Sabino','Chatfield',89874539, 'ChrSabino@gmail.com'),
- ('Jeffrey','Guerrero','Patillo',82979275, 'JefGuerrero@gmail.com'),
- ('Raymond','Foust','Nguyen',82385652, 'RayFoust@gmail.com'),
- ('Deangelo','Considine','Gordon',86830732, 'DeaConsidine@gmail.com'),
- ('Terry','Arnold','Mathew',89605250, 'TerArnold@gmail.com'),
- ('Nina','Westerman','Banda',86079107, 'NinWesterman@gmail.com'),
- ('Ashley','Mckinley','Priolo',85371669, 'AshMckinley@gmail.com'),
- ('Joya','Hathaway','Smith',81029918, 'JoyHathaway@gmail.com'),
- ('Lorraine','Bullock','Keeler',85133678, 'LorBullock@gmail.com'),
- ('Joan','Jones','Madden',84914047, 'JoaJones@gmail.com'),
- ('Kevin','Kent','Black',89180931, 'KevKent@gmail.com'),
- ('Cheryl','Wyman','Mcfarland',85096644, 'CheWyman@gmail.com');
+('Grace','Starcher','Parker' ),
+ ('Tiny','Jones','Mansfield'),
+ ('Evan','Reichenbach','Tran' ),
+ ('Richard','Rush','Gartrell'),
+ ('Lawrence','Bernhart','Settles'),
+ ('Joan','Gosselin','Griffin'),
+ ('Jason','Hoag','Daley' ),
+ ('David','Tung','Sharp'),
+ ('Christina','Sabino','Chatfield'),
+ ('Jeffrey','Guerrero','Patillo'),
+ ('Raymond','Foust','Nguyen'),
+ ('Deangelo','Considine','Gordon'),
+ ('Terry','Arnold','Mathew'),
+ ('Nina','Westerman','Banda'),
+ ('Ashley','Mckinley','Priolo'),
+ ('Joya','Hathaway','Smith'),
+ ('Lorraine','Bullock','Keeler'),
+ ('Joan','Jones','Madden'),
+ ('Kevin','Kent','Black'),
+ ('Cheryl','Wyman','Mcfarland');
  
  -- ---------------------------------------------------------------
  -- Insert data into the Provincias
