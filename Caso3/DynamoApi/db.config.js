479e783f4b719fbb84d293df8a1dc0168b254a75
MarioBarbozaA <marionetabar1@gmail.com>
2023-03-21 02:14:42 -0600

    MarioUsuario

--- Caso3/DynamoApi/db.config.js
+++ Caso3/DynamoApi/db.config.js
@@ -1,9 +1,9 @@
 import AWS from 'aws-sdk'
 
 AWS.config.update({
-    region: "us-east-1",
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    region: "us-east-2",
+    accessKeyId: "AKIAY7NGQZFRW3KEQDXE",//process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: "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" //process.env.AWS_SECRET_ACCESS_KEY
 })
 
 const db = new AWS.DynamoDB.DocumentClient()
